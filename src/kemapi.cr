48de72e0ce11aeb1d9c0809889cf492c3e826a5f
Rishav <rishav.sharan@live.com>
2018-06-14 01:29:05 +0530

    wip: basic model validation

--- src/kemapi.cr
+++ src/kemapi.cr
@@ -3,6 +3,7 @@ require "sentry-run"
 require "dotenv"
 require "pg"
 require "granite/adapter/pg"
+require "crypto/bcrypt/password"
 
 Dotenv.load
 
@@ -10,8 +11,8 @@ require "./models/*"
 
 module Kemapi
 
-    # User.migrator.drop_and_create
-    # Post.migrator.drop_and_create
+    User.migrator.drop_and_create
+    Post.migrator.drop_and_create
 
     before_all do |env|
         puts "Setting response content type"
@@ -35,9 +36,19 @@ module Kemapi
 
     post "/users/" do |env|
         uname = env.params.json["username"].as(String)
-        pass = env.params.json["password_hash"].as(String)
+        pass = env.params.json["password"].as(String)
+        pass_hash = Crypto::Bcrypt::Password.create(pass)
 
-        Post.create(username: uname, password_hash: pass)
+        new_user = User.new(username: uname, password_hash: pass_hash.to_s)
+        new_user.save
+
+        if new_user.errors.size > 0
+            new_user.errors.each do |err|
+                puts err
+            end
+        else
+            puts "No error detected"
+        end
 
     end
 
--- src/models/User.cr
+++ src/models/User.cr
@@ -6,5 +6,13 @@ module Kemapi
         field password_hash : String
         
         timestamps
+
+        # validate_uniqueness :username
+        validate_min_length :username, 3
+        validate_max_length :username, 255
+
+        validate_min_length :password_hash, 60
+        validate_max_length :password_hash, 60
+
     end
 end
